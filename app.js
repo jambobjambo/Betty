801d04e1bdae9ddcd561ae1826c906c38de027ca
jambobjambo <jamie.a.mcinally@gmail.com>
2016-12-02 13:23:26 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.listen((process.env.PORT || 8080));
 // Server frontpage
 app.get('/', function (req, res) {
     var image = new Jimp(780, 410, function (err, image) {
-        image.background('0xFFFFFFFF');
+        image.composite('https://raw.githubusercontent.com/jambobjambo/Betty/master/image/odds1.jpg', 0, 0);
         image.write("test.png");
         res.send('<img src="test.png" />');
         // this image is 256 x 256, every pixel is set to 0x00000000
