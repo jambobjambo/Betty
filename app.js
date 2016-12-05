7a0c99e6feb394a224581d93c160db9c7ec4c51a
jambobjambo <jamie.a.mcinally@gmail.com>
2016-12-05 12:32:24 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -230,7 +230,7 @@ function showodds(recipientId, parameters) {
         GetOddsCurrent('football', function(Match){
             createImage(Match, function(filename, Match){
                 messageTemp.push ('{"title":' + Match[0] + ' v ' + Match[1] + ',"subtitle":' + Match[2] + ',"image_url": "https://chatbettyeu.herokuapp.com/"' + filename + ',"buttons": [{"title": "Place a Bet","type": "postback","payload": "PLACE_BET"},{"title": "Add to Accumulator","type": "postback","payload": "ADD_TO_ACC"},{"title": "Update","type": "postback","payload": "UPDATE"}]}');
-
+                console.log(messageTemp);
                 if(messageTemp.length == 10){
                     message = {
                         "attachment": {
